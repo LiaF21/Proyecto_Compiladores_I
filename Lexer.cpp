645eca642c77e894cb0765844de950b629083cbd
LiaF21 <125204625+LiaF21@users.noreply.github.com>
2025-11-15 22:48:43 -0600

    Lexer 99% completed, check error handling

--- Lexer.cpp
+++ Lexer.cpp
@@ -8,6 +8,12 @@ enum class State{
     IDENTIFIER_Q2,
     IDENTIFIER_Q3,
     SPACES_Q1,
+    EQUALSING_Q1,
+    GREATER_Q1,
+    LESS_Q1,
+    NOT_Q1,
+    AND_Q1,
+    OR_Q1
 };
 
 Token Lexer::nextToken(){
@@ -30,6 +36,66 @@ Token Lexer::nextToken(){
             }else if(currentChar == ' ' || currentChar =='\t'){
                 in.get();
                 state = State::SPACES_Q1;
+            }else if(currentChar == '+'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::ADD;
+            }else if(currentChar == '-'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::SUBS;
+            }else if(currentChar == '/'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::DIV;
+            }else if(currentChar == '*'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::MULT;
+            }else if(currentChar == ';'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::POINTCOMMA;
+            }else if(currentChar == '{'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::OBRACKET;
+            }else if(currentChar == '}'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::CBRACKET;
+            }else if(currentChar == '('){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::OPAREN;
+            }else if(currentChar == ')'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                return Token::CPAREN;
+            }else if(currentChar == '='){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::EQUALSING_Q1;
+            }else if(currentChar == '<'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::LESS_Q1;
+            }else if(currentChar == '>'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::GREATER_Q1;
+            }else if(currentChar == '!'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::NOT_Q1;  
+            }else if(currentChar == '&'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::AND_Q1;
+            }else if(currentChar == '|'){
+                text+= static_cast<char>(currentChar);
+                in.get();
+                state = State::OR_Q1;
             }else{
                 throw std::runtime_error(std::string("Invalid Character ")+ static_cast<char>(currentChar));
             }
@@ -80,6 +146,16 @@ Token Lexer::nextToken(){
                     text+= static_cast<char>(currentChar);
                     currentChar = in.get();
                     state = State::IDENTIFIER_Q3;
+            }else if(text == "int"){
+                return Token::INT;
+            }else if(text == "while"){
+                return Token::WHILE;
+            }else if(text == "if"){
+                return Token::IF;
+            }else if(text == "else"){
+                return Token::ELSE;
+            }else if(text == "print"){
+                return Token::PRINT;
             }else{
                 return Token::IDENTIFIER;
             }
@@ -90,6 +166,60 @@ Token Lexer::nextToken(){
                 state = State::SPACES_Q1;
                 }else{
                     state = State::Q0;
+                }
+            break;
+            case State::EQUALSING_Q1:
+                if(currentChar == '='){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::EQUAL;
+                }else{
+                    return Token::ASSIGN;
+                }
+            break;
+            case State::LESS_Q1:
+                if(currentChar == '='){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::LESSEQUAL;
+                }else{
+                    return Token::LESS;
+                }
+            break;
+            case State::GREATER_Q1:
+                if(currentChar == '='){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::GREATEREQUAL;
+                }else{
+                    return Token::GREATER;
+                }
+            break;
+            case State::NOT_Q1:
+                if(currentChar == '='){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::NOTEQUAL;
+                }else{
+                    return Token::NOT;
+                }
+            break;
+            case State::AND_Q1:
+                if(currentChar == '&'){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::AND;
+                }else{
+                    return Token::ERROR;
+                }
+            break;
+            case State::OR_Q1:
+                if(currentChar == '|'){
+                    text+= static_cast<char>(currentChar);
+                    in.get();
+                    return Token::OR;
+                }else{
+                    return Token::ERROR;
                 }
             break;
         default:
@@ -107,6 +237,56 @@ const char *Lexer::TokenToString(Token token){
         return "Identifier";
     case Token::NUMBER:
         return "Number";
+    case Token::IF:
+        return "If";
+    case Token::WHILE:
+        return "While";
+    case Token::ELSE:
+        return "Else";
+    case Token::PRINT:
+        return "Print"; 
+    case Token::ASSIGN:
+        return "Assign";
+    case Token::GREATER:
+        return "Greater";  
+    case Token::LESS:
+        return "Less";  
+    case Token::GREATEREQUAL:
+         return "Greater Equal";
+    case Token::LESSEQUAL:
+        return "Less Equal";
+    case Token::EQUAL:
+        return "Equal";
+    case Token::NOTEQUAL:
+        return "Not Equal";
+    case Token::NOT:
+        return "Not";
+    case Token::AND:
+        return "And";
+    case Token::OR:
+        return "Or";
+    case Token::OPAREN:
+        return "Open Parenthesis";
+    case Token::CPAREN:
+        return "Close Parenthesis";
+    case Token::ADD:
+        return "Addition";
+    case Token::SUBS:
+        return "Subtraction";
+    case Token::MULT:
+        return "Multiplication";
+    case Token::DIV:
+        return "Division";
+    case Token::OBRACKET:
+        return "Open Bracket";
+    case Token::CBRACKET:
+        return "Close Bracket";
+    case Token::INT:
+        return "Int";
+    case Token::POINTCOMMA:
+        return "Point Comma";
+    case Token::ERROR:
+        return "Error";    
     default:
         return " ";
     }
